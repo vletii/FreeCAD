1ffcd9b902d57daf1ada0e52e751ae74ab642bfb
Leticia Vong <leticia.vong@tecnico.ulisboa.pt>
2025-06-01 22:42:00 +0100

    fix: graph without graphviz and pdf exportation without qprinter

--- src/Mod/Assembly/CommandCreateDependencyMap.py
+++ src/Mod/Assembly/CommandCreateDependencyMap.py
@@ -1,42 +1,24 @@
 import os
 import FreeCAD as App
 
-from PySide.QtCore import QT_TRANSLATE_NOOP
-
 if App.GuiUp:
     import FreeCADGui as Gui
-    from PySide import QtCore, QtWidgets
-    from PySide.QtSvg import QSvgRenderer
-    from PySide.QtWidgets import QMainWindow, QGraphicsView, QGraphicsScene, QFileDialog, QToolBar, QAction, QFileDialog
-    from PySide2.QtGui import QImage, QPainter
-    from PySide2.QtCore import Qt
-    from PySide.QtSvg import QGraphicsSvgItem
-    # from PySide2.QtPrintSupport import QPrinter
-    # import graphviz
+    from PySide2 import QtWidgets, QtCore, QtGui
+    from PySide2.QtWidgets import QMainWindow, QGraphicsView, QGraphicsScene, QFileDialog
+    from PySide2.QtCore import Qt, QPointF
+    from PySide2.QtGui import QPainter, QPen, QBrush, QImage
+    from PySide2.QtSvg import QSvgRenderer, QGraphicsSvgItem
 
+from PIL import Image as PILImage
 import UtilsAssembly
-import Assembly_rc
 
-
-title = "Assembly Command to show Dependency Map"
-author = "Ondsel"
-url = "https://www.freecad.org/"
 
 class CommandCreateDependencyMap:
-    def init(self):
-        pass
-
     def GetResources(self):
-
         return {
-            # TODO change pixmap icon
             "Pixmap": "Assembly_ExportASMT",
-            "MenuText": QT_TRANSLATE_NOOP("Assembly_CreateDependencyMap", "Create a Dependency Map"),
-            # "Accel": "Z", # shortcut key - define later maybe
-            "ToolTip": QT_TRANSLATE_NOOP(
-                "Assembly_CreateDependencyMap",
-                "Create a Dependency Map",
-            ),
+            "MenuText": "Create a Dependency Map",
+            "ToolTip": "Create a Dependency Map",
         }
 
     def IsActive(self):
@@ -46,141 +28,121 @@ class CommandCreateDependencyMap:
         assembly = UtilsAssembly.activeAssembly()
         if not assembly:
             return
-
         panel = TaskAssemblyCreateDependencyMap()
         Gui.Control.showDialog(panel)
 
 class TaskAssemblyCreateDependencyMap(QtCore.QObject):
-    def __init__(self):  
+    def __init__(self):
         super().__init__()
         self.assembly = UtilsAssembly.activeAssembly()
-
         if not self.assembly:
             return
 
-        self.dependency_map = None
         self.form = Gui.PySideUic.loadUi(":/panels/TaskAssemblyCreateDependencyMap.ui")
 
-        # self.form.btnGenerate.clicked.connect(self.renderMap)
-        # self.form.btnExport.clicked.connect(self.exportMap)
+        self.form.btnGenerate.clicked.connect(self.renderMap)
+        self.form.btnExport.clicked.connect(self.exportMap)
 
-    # def renderMap(self):
-    #     # self.g = graphviz.Graph()
-    #     # self.g.attr()
+        self.window = QtWidgets.QMainWindow()
+        self.window.setWindowTitle("Dependency Map")
+        self.scene = QGraphicsScene()
+        self.view = DependencyMapView(self.scene)
+        self.window.setCentralWidget(self.view)
+        self.nodes = {}
 
-    #     # self.addNodesToGraph(self.g)
-    #     # self.addEdgesToGraph(self.g, self.assembly)
+    def renderMap(self):
+        self.scene.clear()
+        self.nodes.clear()
+        start_y = 0
+        self.add_parts(self.assembly, 0, start_y)
+        self.visualize()
 
-    #     self.visualizeMap()
-        
-    # def exportMap(self):
-    #     filters = "PNG (*.png);;JPEG (*.jpg *.jpeg);;Bitmap (*.bmp);;Scalable Vector Graphics (*.svg);;PDF (*.pdf)"
-    #     path, selected_filter = QFileDialog.getSaveFileName(None, "Export Dependency Map", "", filters)
+    def add_parts(self, assembly, x, y, parent_pos=None):
+        label = assembly.Name
+        pos = QPointF(x, y)
+        rect = self.scene.addRect(x, y, 120, 40, QtGui.QPen(Qt.black), QtGui.QBrush(Qt.lightGray))
+        text = self.scene.addText(label)
+        text.setPos(x + 10, y + 10)
+        self.nodes[label] = pos
 
-    #     if not path:
-    #         return
+        if parent_pos:
+            self.drawEdge(parent_pos, pos)
 
-    #     if "PNG" in selected_filter:
-    #         fmt = "PNG"
-    #     elif "JPEG" in selected_filter:
-    #         fmt = "JPEG"
-    #     elif "Bitmap" in selected_filter:
-    #         fmt = "BMP"
-    #     elif "Scalable" in selected_filter:
-    #         fmt = "SVG"
-    #     elif "PDF" in selected_filter:
-    #         fmt = "pdf"
+        y_offset = y + 120
+        for part in UtilsAssembly.getParts(assembly):
+            p_label = part.Label
+            part_pos = QPointF(x + 160, y_offset)
+            rect = self.scene.addRect(part_pos.x(), part_pos.y(), 100, 40, QtGui.QPen(Qt.black), QtGui.QBrush(Qt.cyan))
+            text = self.scene.addText(p_label)
+            text.setPos(part_pos.x() + 10, part_pos.y() + 10)
+            self.nodes[p_label] = part_pos
+            self.drawEdge(pos, part_pos)
+            y_offset += 120
 
-    #     if not path.lower().endswith(f".{fmt.lower()}"):
-    #         path += f".{fmt.lower()}"
+        for sub in UtilsAssembly.getSubAssemblies(assembly):
+            y_offset += 40
+            self.add_parts(sub, x + 160, y_offset, pos)
 
-    #     if fmt == "SVG":
-    #         with open(path, "wb") as f:
-    #             f.write(self.svg_data)
+        # Draw edges for joints
+        for joint in self.assembly.Joints:
+            part1 = UtilsAssembly.getMovingPart(self.assembly, joint.Reference1)
+            part2 = UtilsAssembly.getMovingPart(self.assembly, joint.Reference2)
+            if part1 and part2 and part1.Label in self.nodes and part2.Label in self.nodes:
+                self.drawEdge(self.nodes[part1.Label], self.nodes[part2.Label], color=Qt.red)
 
-    #     elif fmt == "pdf":
-    #         renderer = QSvgRenderer(self.svg_data)
-    #         printer = QPrinter()
-    #         printer.setOutputFormat(QPrinter.PdfFormat)
-    #         printer.setOutputFileName(path)
 
-    #         bounds = renderer.viewBoxF()
-    #         printer.setPaperSize(bounds.size(), QPrinter.Point)
-    #         printer.setFullPage(True)
+    def drawEdge(self, start, end, color=Qt.black):
+        line = QtCore.QLineF(start + QPointF(60, 20), end + QPointF(0, 20))
+        pen = QtGui.QPen(color, 2)
+        self.scene.addLine(line, pen)
 
-    #         painter = QPainter(printer)
-    #         renderer.render(painter)
-    #         painter.end()
 
-    #     else:
-    #         self.renderer = QSvgRenderer(self.svg_data)
-    #         bounds = self.renderer.viewBoxF()
-    #         size = bounds.size().toSize()
+    def visualize(self):
+        mdi_area = Gui.getMainWindow().findChild(QtWidgets.QMdiArea)
+        if mdi_area:
+            sub_window = mdi_area.addSubWindow(self.view)
+            sub_window.show()
+        else:
+            self.window.show()
 
-    #         if size.width() == 0 or size.height() == 0:
-    #             return
+    def exportMap(self):
+        filters = "PNG (*.png);;JPEG (*.jpg *.jpeg);;Bitmap (*.bmp);;PDF (*.pdf)"
+        path, selected_filter = QFileDialog.getSaveFileName(None, "Export Dependency Map", "", filters)
 
-    #         image = QImage(size, QImage.Format_ARGB32)
-    #         image.fill(Qt.transparent)
+        if not path:
+            return
 
-    #         painter = QPainter(image)
-    #         self.renderer.render(painter)
-    #         painter.end()
+        if "PNG" in selected_filter:
+            fmt = "PNG"
+        elif "JPEG" in selected_filter:
+            fmt = "JPEG"
+        elif "Bitmap" in selected_filter:
+            fmt = "BMP"
+        elif "Scalable" in selected_filter:
+            fmt = "SVG"
+        elif "PDF" in selected_filter:
+            fmt = "pdf"
 
-    # def addNodesToGraph(self, g):
-    #     assembly = UtilsAssembly.activeAssembly()
-    #     subassembly = UtilsAssembly.getSubAssemblies(assembly)
-    #     for sub in subassembly:
-    #         self.addsSubGraphNodes(g, sub)
-    #     with g.subgraph(name = 'cluster_0 ') as s:
-    #         for part in UtilsAssembly.getParts(assembly):
-    #             s.node(part.Name, style="filled", fillcolor="lightgrey")
-        
+        if not path.lower().endswith(f".{fmt.lower()}"):
+            path += f".{fmt.lower()}"
 
-    # def addsSubGraphNodes(self, g, assembly):
-    #     #subgraph
-    #     with g.subgraph(name = 'cluster_' + assembly.Name) as s:
-    #         s.attr(style="filled", color= "lightpink", label=assembly.Name)
-    #         subassembly = UtilsAssembly.getSubAssemblies(assembly)
-    #         for sub in subassembly:
-    #             self.addsSubGraphNodes(g, sub)
-    #         for obj in UtilsAssembly.getParts(assembly):
-    #             s.node(obj.Label, label=obj.Label,  style="filled", fillcolor="lightblue")
+        rect = self.scene.sceneRect()
 
-    # def addEdgesToGraph(self, g, assembly):
-    #     joints = assembly.Joints
-    #     for joint in joints:
-    #         #g.node(joint.Label, label=joint.Label, style="filled", fillcolor = "green",shape='Mdiamond')
-    #         part1 = UtilsAssembly.getMovingPart(assembly, joint.Reference1)
-    #         part2 = UtilsAssembly.getMovingPart(assembly, joint.Reference2)
-    #         if part1 and part2:
-    #             # g.edge(part1.Label, joint.Label)
-    #             # g.edge(joint.Label, part2.Label)
-                
-    #             # if self.form.CheckBox_ShowJoints.isChecked(): # if show joints
-    #                 # print("show joints enabled")
-    #             #     g.node(joint.Label, label=joint.Label, style="filled", fillcolor = "green",shape='Mdiamond')
-    #             #     g.edge(part1.Label, joint.Label)
-    #             #     g.edge(joint.Label, part2.Label)
-    #             # else:
-    #             g.edge(part1.Label, part2.Label)
+        image = QImage(rect.size().toSize(), QImage.Format_ARGB32)
+        image.fill(Qt.white)
+        painter = QPainter(image)
+        self.scene.render(painter)
+        painter.end()
 
-    # def visualizeMap(self):
-        # self.svg_data = self.g.pipe(format="svg")
+        if fmt == "pdf": # Save as temporary PNG, then convert to PDF using Pillow
+            temp_png = path[:-4] + "_temp_export.png"
+            image.save(temp_png)
+            pil_img = PILImage.open(temp_png)
+            pil_img.save(path, "PDF", resolution=100.0)
+            os.remove(temp_png)
 
-        # mdi_area = Gui.getMainWindow().findChild(QtWidgets.QMdiArea)
-
-        # if not mdi_area: 
-        #     return
-
-        # if self.dependency_map:
-        #     self.dependency_map.updateSvg(self.svg_data)
-        #     self.dependency_map.raise_()
-        #     self.dependency_map.activateWindow()
-        # else:
-        #     self.dependency_map = GraphvizSvgView(self.svg_data, self.dependency_map)
-        #     sub_window = mdi_area.addSubWindow(self.dependency_map)
-        #     sub_window.show()
+        else:
+            image.save(path)
 
     def accept(self):
         self.deactivate()
@@ -195,23 +157,12 @@ class TaskAssemblyCreateDependencyMap(QtCore.QObject):
             Gui.Control.closeDialog()
 
 
-class GraphvizSvgView(QMainWindow):
-    def __init__(self, svg_data, dependency_map, parent=None):
-        super().__init__(parent)
-        self.dependency_map = dependency_map
-        self.setWindowTitle("Dependency Graph")
+class DependencyMapView(QGraphicsView):
+    def __init__(self, scene, parent=None):
+        super().__init__(scene, parent)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        
         self._zoom = 0
-
-        self.scene = QGraphicsScene(self)
-        self.renderer = QSvgRenderer(svg_data, self)
-        self.svg_item = QGraphicsSvgItem()
-        self.svg_item.setSharedRenderer(self.renderer)
-        self.scene.addItem(self.svg_item)
-
-        self.view = QGraphicsView(self.scene, self)
-        self.view.setDragMode(QGraphicsView.ScrollHandDrag)
-
-        self.setCentralWidget(self.view)
 
     def wheelEvent(self, event):
         zoomInFactor = 1.25
@@ -224,12 +175,12 @@ class GraphvizSvgView(QMainWindow):
             zoomFactor = zoomOutFactor
             self._zoom -= 1
 
-        self.view.scale(zoomFactor, zoomFactor)
-    
-    def updateSvg(self, svg_data):
-        self.renderer.load(svg_data)
-        self.view.resetTransform()
+        self.scale(zoomFactor, zoomFactor)
+
+    def mouseDoubleClickEvent(self, event):
+        self.resetTransform()
         self._zoom = 0
+        super().mouseDoubleClickEvent(event)
 
 
 if App.GuiUp:
